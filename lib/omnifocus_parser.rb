09db44e0cb59b8ebe0693b44986a8ba9378135bd
Andy Waite <andyw8@users.noreply.github.com>
2022-12-25 14:34:55 -0500

    $ bundle gem omnifocus_parser

---
+++ lib/omnifocus_parser.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "omnifocus_parser/version"
+
+module OmnifocusParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_omnifocus_parser.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestOmnifocusParser < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::OmnifocusParser::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
