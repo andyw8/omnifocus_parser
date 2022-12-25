f56e7e4f307c65038eab8fd749d3ffbea9faca12
Andy Waite <andyw8@users.noreply.github.com>
2022-12-25 14:45:24 -0500

    Remove example failing test

--- test/test_omnifocus_parser.rb
+++ test/test_omnifocus_parser.rb
@@ -6,8 +6,4 @@ class TestOmnifocusParser < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::OmnifocusParser::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
